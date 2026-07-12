55748785f3f5785a39b0a4e1f9e791849158eb1c
eliasjuk <eliasp.juk@hotmail.com>
2026-07-12 19:59:19 -0300

    refactor(app): initialize database on startup

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:factory_flow/app.dart';
+import 'package:factory_flow/core/config/app_paths.dart';
+import 'package:factory_flow/core/database/database_manager.dart';
 import 'package:flutter/material.dart';
 
-import 'app.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await AppPaths.initialize();
+  await DatabaseManager.initialize();
+
   runApp(const FactoryFlowApp());
 }
